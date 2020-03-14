148fbdeb91be6b1078b9b561d42251be63c31bf7
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2020-03-14 15:38:07 -0300

    Initial project

---
+++ src/main/java/com/jovanibrasil/ApisAdminApplication.java
@@ -0,0 +1,13 @@
+package com.jovanibrasil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jovanibrasil/ApisAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jovanibrasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
